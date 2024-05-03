36932f02d5679f1ea1b9f813cbf5889515b047a6
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-05-03 14:21:22 +0200

    Add Docu

--- model/Weekday.java
+++ model/Weekday.java
@@ -1,5 +1,12 @@
 package model;
 
+/**
+ * Wochentag
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
 public enum Weekday {
 
   MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
